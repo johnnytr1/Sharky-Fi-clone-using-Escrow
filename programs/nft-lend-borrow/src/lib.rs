ad5c6e9cc4778bd3c84f6942d13f49f80c2b8047
johnnytr1 <158827427+johnnytr1@users.noreply.github.com>
2024-02-04 22:06:15 +0700

    declare lending program id

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("EN5ynizCn9ne4LM66ycXX8d8nBpdzppfdytSsXJTKBNX");
 
 #[program]
 pub mod nft_lend_borrow {
